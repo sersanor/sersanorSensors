5b229e72316fd4dd96a341d0ff457ca3c0a6cb01
Sergio Sánchez Ortiz <sersanor@gmail.com>
2014-12-01 02:14:40 +0100

    1st Commit

---
+++ src/ffm/sersanorsensors/MainActivity.java
@@ -0,0 +1,186 @@
+package ffm.sersanorsensors;
+
+import java.util.Iterator;
+import java.util.List;
+
+import android.support.v7.app.ActionBarActivity;
+import android.content.Context;
+import android.content.Intent;
+import android.hardware.Sensor;
+import android.hardware.SensorManager;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+
+
+public class MainActivity extends ActionBarActivity {
+	CheckBox s1,s2,s3,s4,s5,s6,s7,s8,s9;
+	Button b1,b2,b3,b4,b5,b6,b7,b8,b9;
+	private SensorManager mSensorManager;
+	
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //initialize vars
+        init();
+        //check sensors availability
+        checkSensors();
+    }
+    public void init(){
+    	s1 = (CheckBox)findViewById((R.id.s1_CB));
+    	s2 = (CheckBox)findViewById((R.id.s2_CB));
+    	s3 = (CheckBox)findViewById((R.id.s3_CB));
+    	s4 = (CheckBox)findViewById((R.id.s4_CB));
+    	s5 = (CheckBox)findViewById((R.id.s5_CB));
+    	s6 = (CheckBox)findViewById((R.id.s6_CB));
+    	s7 = (CheckBox)findViewById((R.id.s7_CB));
+    	s8 = (CheckBox)findViewById((R.id.s8_CB));
+    	s9 = (CheckBox)findViewById((R.id.s9_CB));
+    	
+    	b1 = (Button)findViewById((R.id.b1));
+    	b2 = (Button)findViewById((R.id.b2));
+    	b3 = (Button)findViewById((R.id.b3));
+    	b4 = (Button)findViewById((R.id.b4));
+    	b5 = (Button)findViewById((R.id.b5));
+    	b6 = (Button)findViewById((R.id.b6));
+    	b7 = (Button)findViewById((R.id.b7));
+    	b8 = (Button)findViewById((R.id.b8));
+    	b9 = (Button)findViewById((R.id.b9));
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+    
+    
+    // SENSORS TEST+
+    
+    public void checkSensors(){
+        SensorManager mgr = (SensorManager) getSystemService(SENSOR_SERVICE);
+        List<Sensor> sensors = mgr.getSensorList(Sensor.TYPE_ALL);
+        for (Sensor sensor : sensors) {
+            Log.d("Sensors", "" + sensor.getName());
+        } 	
+        
+        // S1
+        if (mgr.getDefaultSensor(Sensor.TYPE_ACCELEROMETER) != null){
+        	s1.setChecked(true);
+        	b1.setEnabled(true);
+        	} 
+        else {
+        		s1.setChecked(false);
+        		b1.setEnabled(false);
+        	}
+        
+        // S2
+        if (mgr.getDefaultSensor(Sensor.TYPE_LIGHT) != null){
+        	s2.setChecked(true);
+        	b2.setEnabled(true);
+        	} 
+        else {
+        		s2.setChecked(false);
+        		b2.setEnabled(false);
+        	}
+        
+        // S3
+        if (mgr.getDefaultSensor(Sensor.TYPE_ORIENTATION) != null){
+        	s3.setChecked(true);
+        	b3.setEnabled(true);
+        	} 
+        else {
+        		s3.setChecked(false);
+        		b3.setEnabled(false);
+        	}
+        
+        // S4
+        if (mgr.getDefaultSensor(Sensor.TYPE_PROXIMITY) != null){
+        	s4.setChecked(true);
+        	b4.setEnabled(true);
+        	} 
+        else {
+        		s4.setChecked(false);
+        		b4.setEnabled(false);
+        	}
+        
+        // S5
+        if (mgr.getDefaultSensor(Sensor.TYPE_TEMPERATURE) != null){
+        	s5.setChecked(true);
+        	b5.setEnabled(true);
+        	} 
+        else {
+        		s5.setChecked(false);
+        		b5.setEnabled(false);
+        	}
+        
+        // S6
+        if (mgr.getDefaultSensor(Sensor.TYPE_GYROSCOPE) != null){
+        	s6.setChecked(true);
+        	b6.setEnabled(true);
+        	} 
+        else {
+        		s6.setChecked(false);
+        		b6.setEnabled(false);
+        	}
+        
+        // S7
+        if (true){ //TODO: CHECK ANDROID VERSION
+        	s7.setChecked(true);
+        	b7.setEnabled(true);
+        	} 
+        else {
+        		s7.setChecked(false);
+        		b7.setEnabled(false);
+        	}
+        
+        // S8
+        if (mgr.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD) != null){
+        	s8.setChecked(true);
+        	b8.setEnabled(true);
+        	} 
+        else {
+        		s8.setChecked(false);
+        		b8.setEnabled(false);
+        	}
+        
+        //S9
+        
+        if (mgr.getDefaultSensor(Sensor.TYPE_PRESSURE) != null){
+        	s9.setChecked(true);
+        	b9.setEnabled(true);
+        	} 
+        else {
+        		s9.setChecked(false);
+        		b9.setEnabled(false);
+        	}
+        
+    }
+    
+    // LAUNCH NEW ACTIVITYS
+    
+    public void launchS1(View view){
+    	Intent intent = new Intent(this, S1Activity.class);
+    	startActivity(intent);
+    }
+    
+    
+}
---
+++ src/ffm/sersanorsensors/S1Activity.java
@@ -0,0 +1,176 @@
+package ffm.sersanorsensors;
+
+import java.text.DecimalFormat;
+import java.util.ArrayList;
+import java.util.Set;
+
+import android.support.v7.app.ActionBarActivity;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.content.Context;
+import android.content.Intent;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class S1Activity extends ActionBarActivity implements
+		SensorEventListener {
+
+	private SensorManager senSensorManager;
+	private Sensor senAccelerometer;
+	private BluetoothAdapter BA;
+	TextView vx, vy, vz, name, vendor, version, power;
+	Button On, Off, Visible, list;
+	ListView lv;
+	private Set<BluetoothDevice>pairedDevices;
+
+	protected void onResume() {
+		super.onResume();
+		senSensorManager.registerListener(this, senAccelerometer,
+				SensorManager.SENSOR_DELAY_NORMAL);
+	}
+
+	protected void onPause() {
+		super.onPause();
+		senSensorManager.unregisterListener(this);
+	}
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_s1);
+		init();
+		senSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+		senAccelerometer = senSensorManager
+				.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+		senSensorManager.registerListener(this, senAccelerometer,
+				SensorManager.SENSOR_DELAY_NORMAL);
+
+		sensorInfo();
+	}
+
+	public void init() {
+		vx = (TextView) findViewById(R.id.vxT);
+		vy = (TextView) findViewById(R.id.vyT);
+		vz = (TextView) findViewById(R.id.vzT);
+		vendor = (TextView) findViewById(R.id.s_vendor);
+		power = (TextView) findViewById(R.id.s_power);
+		version = (TextView) findViewById(R.id.s_version);
+		name = (TextView) findViewById(R.id.s_name);
+
+		// BT
+		On = (Button) findViewById(R.id.Button01);
+		Off = (Button) findViewById(R.id.Button02);
+		Visible = (Button) findViewById(R.id.button03);
+		list = (Button) findViewById(R.id.button04);
+		lv = (ListView) findViewById(R.id.listView1);
+		BA = BluetoothAdapter.getDefaultAdapter();
+	}
+
+	public void sensorInfo() {
+		SensorManager sm = (SensorManager) getSystemService(SENSOR_SERVICE);
+		Sensor acc = sm.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+
+		name.setText(" " + acc.getName());
+		vendor.setText(" " + acc.getVendor());
+		version.setText(" " + Integer.toString(acc.getVersion()));
+		power.setText(" " + Float.toString(acc.getPower()) + " mA");
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.s1, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+
+	@Override
+	public void onAccuracyChanged(Sensor arg0, int arg1) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void onSensorChanged(SensorEvent sensorEvent) {
+		// TODO Auto-generated method stub
+		Sensor mySensor = sensorEvent.sensor;
+		DecimalFormat dec = new DecimalFormat("0.000");
+
+		if (mySensor.getType() == Sensor.TYPE_ACCELEROMETER) {
+			double x = sensorEvent.values[0];
+			double y = sensorEvent.values[1];
+			double z = sensorEvent.values[2];
+
+			vx.setText(dec.format(x));
+			vy.setText(dec.format(y));
+			vz.setText(dec.format(z));
+		}
+	}
+
+	public void captureBT(View view) {
+		Intent turnOn = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+		startActivityForResult(turnOn, 0);
+	}
+
+	public void on(View view) {
+		if (!BA.isEnabled()) {
+			Intent turnOn = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+			startActivityForResult(turnOn, 0);
+			Toast.makeText(getApplicationContext(), "Turned on",
+					Toast.LENGTH_LONG).show();
+		} else {
+			Toast.makeText(getApplicationContext(), "Already on",
+					Toast.LENGTH_LONG).show();
+		}
+	}
+
+	public void off(View view) {
+		BA.disable();
+		Toast.makeText(getApplicationContext(), "Turned off", Toast.LENGTH_LONG)
+				.show();
+	}
+
+	public void visible(View view) {
+		Intent getVisible = new Intent(
+				BluetoothAdapter.ACTION_REQUEST_DISCOVERABLE);
+		startActivityForResult(getVisible, 0);
+
+	}
+
+	public void list(View view) {
+		pairedDevices = BA.getBondedDevices();
+
+		ArrayList list = new ArrayList();
+		for (BluetoothDevice bt : pairedDevices)
+			list.add(bt.getName());
+
+		Toast.makeText(getApplicationContext(), "Showing Paired Devices",
+				Toast.LENGTH_SHORT).show();
+		final ArrayAdapter adapter = new ArrayAdapter(this,
+				android.R.layout.simple_list_item_1, list);
+		lv.setAdapter(adapter);
+
+	}
+}
